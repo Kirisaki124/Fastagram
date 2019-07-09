7593c7dcf0267b1d9f25351b3ae3ceb834631fcc
Danteproto <stalke4rhunter@gmail.com>
2019-07-09 08:56:05 +0700

    DataAccess

---
+++ Fastagram/App_Code/Manager.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Fastagram.App_Code
+{
+    public class Manager
+    {
+        public static bool IsUserValid(string userName, string password)
+        {
+            return DataAccess.IsUserValid(userName, password);
+        }
+    }
+}
