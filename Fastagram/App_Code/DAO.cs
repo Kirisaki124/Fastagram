e73aef69b6976353075e3c23261a015283b5fddf
TranHoang <tranhoang345@gmail.com>
2019-07-09 09:49:18 +0700

    Update login and register

--- Fastagram/App_Code/DAO.cs
+++ Fastagram/App_Code/DAO.cs
@@ -33,7 +33,7 @@ namespace Fastagram.App_Code
         }
         public static bool IsUserValid(string userName, string password)
         {
-            string sql = "select * from User where Username = @un and Password = @pw";
+            string sql = "select * from [User] where Username = @un and Password = @pw";
             SqlParameter para1 = new SqlParameter("@un", SqlDbType.NVarChar);
             para1.Value = userName;
             SqlParameter para2 = new SqlParameter("@pw", SqlDbType.NVarChar);
